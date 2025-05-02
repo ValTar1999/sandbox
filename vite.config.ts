dce091fe4b6abc418c4f604a1a2621ba86384583
Vladislav <www.vladtarna25@gmail.com>
2025-05-02 09:44:16 +0300

    up date vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sandbox/',
   plugins: [
     tailwindcss(),
     tsconfigPaths()
   ],
-  base: '/sandbox/',
 })
